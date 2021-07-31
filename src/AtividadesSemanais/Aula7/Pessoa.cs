4ce5d52883702dd89eaa5269a07bee925b82ace9
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-07-31 20:20:33 -0300

    B2 - Exercício 1

---
+++ src/AtividadesSemanais/Aula7/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula7 { 
+    public class Program
+    {
+        public static void Main()
+        {
+            Pessoa p1 = new Pessoa();
+            Console.WriteLine("--- Seus dados de Cadastro ---");
+            Console.WriteLine("Nome: " + p1.getNome());
+            Console.WriteLine("Sobrenome: " + p1.getSobrenome());
+            Console.WriteLine("Idade: " + p1.getIdade());
+            Console.WriteLine("Altura: " + p1.getAltura());
+            Console.WriteLine("Peso: " + p1.getPeso());
+        }
+    }
+}
