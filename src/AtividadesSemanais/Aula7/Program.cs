c1345bcfbba24fbeaf81c9e464fe46b64c2851d5
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-07-31 20:31:26 -0300

    B2 - Exercício 1

---
+++ src/AtividadesSemanais/Aula7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula7
+{
+    public class Pessoa
+    {
+        private string nome = "Katharine";
+        private string sobrenome = "Fernandes";
+        private byte idade = 17;
+        private float altura = 1.7F;
+        private float peso = 55.5F;
+
+        //acessando atributos privados
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public byte getIdade()
+        {
+            return this.idade;
+        }
+        public float getAltura()
+        {
+            return this.altura;
+        }
+        public float getPeso()
+        {
+            return this.peso;
+        }
+    }
+}
