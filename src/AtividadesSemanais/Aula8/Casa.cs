f7f6b6f9d827a0705460755c0eb8da9c8b7e8db9
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-08-16 22:04:38 -0300

    Aula 8

---
+++ src/AtividadesSemanais/Aula8/Casa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula8
+{
+    class Casa
+	{
+		public float Area { get; set; }
+		public float Altura { get; set; }
+		public decimal Preco { get; set; }
+
+	}
+}
---
+++ src/AtividadesSemanais/Aula8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula8
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Casa casa = new Casa();
+			Casa casa2 = new Casa();
+
+			casa.Area = 225F;
+			casa.Altura = 20F;
+			casa.Preco = 500000.00m;
+
+			casa2.Area = 500F;
+			casa2.Altura = 40F;
+			casa2.Preco = 1000000.00m;
+
+			Console.WriteLine("--- Casa 1 ---");
+			Console.WriteLine("Area: " + casa.Area);
+			Console.WriteLine("Altura: " + casa.Altura);
+			Console.WriteLine("Preco: " + casa.Preco);
+
+			Console.WriteLine("--- Casa 2 ---");
+			Console.WriteLine("Area: " + casa.Area);
+			Console.WriteLine("Altura: " + casa.Altura);
+			Console.WriteLine("Preco: " + casa.Preco);
+		}
+	}
+}
